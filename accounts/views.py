a5c79e2be9b881e5c2168022334ea29693ea00b5
Oyegbule <oyegbuleeniola04@gmail.com>
2026-04-21 21:26:58 +0100

    Add password toggle and fix login issues

--- accounts/views.py
+++ accounts/views.py
@@ -1,44 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import StaffRegistrationForm
 from django.contrib.auth import authenticate, login, logout
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None and user.is_active:
-            login(request, user)
-            messages.success(request, f'Welcome! You are logged in as {user}')
-            return redirect('dashboard')
-        else:
-            messages.warning(request, 'Something went wrong. Please check form input')
-            return redirect('login')
-    return render(request, 'accounts/login.html') 
-
-def logout_user(request):
-    logout(request)
-    messages.success(request, "Your active session has ended. Log in to continue")
-    return redirect('login')  
-
+from .forms import StaffRegistrationForm  # Import your new form
 
 def register_user(request):
     if request.method == 'POST':
-        # Use StaffRegistrationForm instead of UserCreationForm
         form = StaffRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now login.')
+            messages.success(request, 'Account created! You can now login.')
             return redirect('login')
-        else:
-            # If the form is invalid, re-render the page with errors
-            return render(request, 'accounts/register.html', {'form': form})
     else:
-        # Initial empty form
         form = StaffRegistrationForm()
-        
     return render(request, 'accounts/register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        u_name = request.POST.get('username')
+        p_word = request.POST.get('password')
+        
+        user = authenticate(request, username=u_name, password=p_word)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome back, {user.username}!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return redirect('login')
+
